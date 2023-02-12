6d24f1d6c2cf1994705f412ae4e020a3716aabd3
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2023-02-12 11:31:23 -0500

    created db

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,24 @@
 DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE IF ONLY NOT EXISTS company_db;
+CREATE DATABASE IF NOT EXISTS company_db;
 
 USE company_db;
 
-CREATE TABLE deparments (
-  id INT NOT NULL,
-  name VARCHAR(30)
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  movie_name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE roles (
-  id INT,
-  title VARCHAR(30) NOT NULL,
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  job_title VARCHAR(30),
   salary DECIMAL NOT NULL,
-  department_id INT,
+  department_id INT
 );
 
 CREATE TABLE employee (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT NOT NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
 );
